d226e0df266ebbb8dcfcf75766a6fd6f61fd4ae7
dev-9820 <dev.bhanushali03@gmail.com>
2024-08-27 19:00:06 +0530

    New Update

--- pages/works.js
+++ pages/works.js
@@ -11,35 +11,25 @@ const Works = () => {
     return(
         <Layout>
         <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Portfolio
+            <Heading as={"h1"} fontSize={20} mb={1}>
+                Websites
             </Heading>
-            
-            <SimpleGrid column={[1,1,2]} gap={6}>
-                <Section delay={0.1}>
-                    <Box>
-                    <Button shadow="lg" rightIcon={<ChevronRightIcon/>} colorScheme="teal">
-                        <NextLink href={"https://devb3.itch.io/"}>
-                            Games
-                        </NextLink>
-                    </Button>
-                    </Box>
-                    <Box pt={"5"} flexShrink={0} mt={{ base:4, md: 0}} ml={{md:6}} align="left">
-                <Image shadow='lg' borderColor="whiteAlpha.800" borderWidth={1} borderStyle="solid" maxWidth="250px" display="inline-block" borderRadius="none" src="/images/game1.png" alt="Game1"/>
 
-            </Box>
-                </Section>
-                <Section delay={0.2}>
-                    <Box>
-                    <Button shadow="lg" rightIcon={<ChevronRightIcon/>} colorScheme="teal">
-                        <NextLink href={""}>
-                            Websites
-                        </NextLink>
-                    </Button>
-                    </Box>
+            <SimpleGrid column={[2,2,2]} gap={6}>
+                <Section delay={0.1}>
+                    <NextLink href={"/"} passHref>
                     <Box pt={"5"} flexShrink={0} mt={{ base:4 , md: 0}} ml={{md:6}} align="left">
-                <Image shadow='lg' borderColor="whiteAlpha.800" borderWidth={1} borderStyle="solid" maxWidth="250px" display="inline-block" borderRadius="none" src="/images/website1.png" alt="website1"/>
-                </Box>
+                        <Image shadow='lg' borderColor="whiteAlpha.500" borderWidth={1} borderStyle="solid" maxWidth="250px" display="inline-block" borderRadius="none" src="/images/website1.png" alt="website1"/>
+                        <p>Portfolio Website</p>
+                    </Box>
+                    </NextLink>
+
+                    <NextLink href={"https://data-visualisation-sobv.onrender.com"}>
+                    <Box pt={"5"} flexShrink={0} mt={{ base:4 , md: 0}} ml={{md:6}} align="left">
+                        <Image shadow='lg' borderColor="whiteAlpha.500" borderWidth={1} borderStyle="solid" maxWidth="250px" display="inline-block" borderRadius="none" src="/images/website2.png" alt="website1"/>
+                        <p>Data Visualisation from API</p>
+                    </Box>
+                    </NextLink>
                 </Section>
             </SimpleGrid>
         </Container>
