e2678375836c2aea5b19a98656c0e397d9fc1583
dev-9820 <dev.bhanushali03@gmail.com>
2026-01-31 18:02:09 +0530

    Update fonts.js

--- components/fonts.js
+++ components/fonts.js
@@ -4,7 +4,11 @@ const Fonts = () => (
     <Global
     styles={`
     @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap');
     `}
+    
     />
 )
 
